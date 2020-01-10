7f01a4436b9b3a4b82a2f227432299457b1e18c6
Diego Augusto <diego.augusto@neemu.com>
2020-01-10 13:34:27 -0300

    create main section

---
+++ client/src/MainSection/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface MainSectionProps {}
+
+const MainSection = (props: MainSectionProps): JSX.Element => {
+  return <section id="main-section"></section>;
+};
+
+export default MainSection;
