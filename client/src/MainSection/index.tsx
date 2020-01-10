ef10418426471b8eae4da219cece025d81ee4dc8
Diego Augusto <diego.augusto@neemu.com>
2020-01-10 13:39:25 -0300

    create render children props

--- client/src/MainSection/index.tsx
+++ client/src/MainSection/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-interface MainSectionProps {}
+interface MainSectionProps {
+  children: JSX.Element[];
+}
 
 const MainSection = (props: MainSectionProps): JSX.Element => {
-  return <section id="main-section"></section>;
+  const children = props.children;
+
+  return (
+    <section id="main-section">
+      {React.Children.map(children, (child, i) => {
+        return <div id={`child-${i}`}>{child}</div>;
+      })}
+    </section>
+  );
 };
 
 export default MainSection;
