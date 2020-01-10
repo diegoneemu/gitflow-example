df757d2c94a02d2e28e45db9f7b216c631ec785a
Diego Augusto <diego.augusto@neemu.com>
2020-01-10 16:01:23 -0300

    Feature/014 create home component (#13)
    Squashed commit of the following:
    
    commit 5e8226b03f6155762bba50a2f58d8096833cd2b8
    Author: Diego Augusto <diego.augusto@neemu.com>
    Date:   Fri Jan 10 15:59:21 2020 -0300
    
        fix message at component
    
    commit 487eeb822395986a1c577d54cda5e48613fcd32d
    Author: Diego Augusto <diego.augusto@neemu.com>
    Date:   Fri Jan 10 15:58:57 2020 -0300
    
        create home component

---
+++ client/src/Home/index.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const Home: FunctionComponent<{}> = ({}): JSX.Element => {
+  return <div id="home">Welcome to Git Flow!!!!</div>
+}
+
+export default Home;
